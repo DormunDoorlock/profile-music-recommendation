b96cfb088613cc05bbbdf598dcbdd02c89cc3b6b
unqeye <salyuan@naver.com>
2022-09-21 00:44:00 +0900

    feat: 감정 분석 결과 저장

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Kakao from "./Kakao";
 import Spotify from "./Spotify";
 import Face from "./Face";
 
 export default function App() {
+  const [expressions, setExpressions] = React.useState({});
+
+  useEffect(() => {
+    console.log(expressions);
+  }, [expressions]);
+
   return (
     <>
       <Kakao />
-      <Spotify />
-      <Face />
+      <Spotify expressions={expressions} />
+      <Face setExpressions={setExpressions} />
     </>
   );
 }
--- src/Face.js
+++ src/Face.js
@@ -1,25 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
+import PropTypes from "prop-types";
 import * as faceapi from "face-api.js";
 
-export default function Face() {
+export default function Face({ setExpressions }) {
   useEffect(() => {
     Promise.all([
       faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
       faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
       faceapi.nets.faceExpressionNet.loadFromUri("/models"),
     ]);
-  });
+  }, []);
 
-  const detectFace = async () => {
+  const detectFace = useCallback(async () => {
     const canvas = document.querySelector("canvas");
     const detections = await faceapi
       .detectSingleFace(canvas)
       .withFaceLandmarks()
       .withFaceExpressions();
     console.log(detections);
+    setExpressions(detections.expressions);
     faceapi.draw.drawDetections(canvas, detections);
     faceapi.draw.drawFaceExpressions(canvas, detections);
-  };
+  }, []);
 
   return <div onClick={detectFace}>Detect Face</div>;
 }
+
+Face.propTypes = {
+  setExpressions: PropTypes.func.isRequired,
+};
