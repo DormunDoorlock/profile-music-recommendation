4febbfcd6012170d9966a237fc2783b80ea3f6ea
unqeye <salyuan@naver.com>
2022-09-22 16:27:55 +0900

    feat: 추천 음악 리스트 출력

--- frontend/src/Spotify.js
+++ frontend/src/Spotify.js
@@ -1,17 +1,45 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 
 export default function Spotify({ expressions }) {
+  const [tracks, setTracks] = useState([]);
+
   const getRecommendedMusicList = useCallback(() => {
-    fetch("https://480ytpc372.execute-api.ap-northeast-2.amazonaws.com/dev/handler/musicRmd", {
+    fetch("https://avi5pau41d.execute-api.ap-northeast-2.amazonaws.com/dev/handler/musicRec", {
       method: "POST",
       body: JSON.stringify(expressions),
     })
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setTracks(data.tracks));
   }, []);
 
-  return <div onClick={getRecommendedMusicList}>Get Recommended Music List</div>;
+  return (
+    <>
+      <div onClick={getRecommendedMusicList}>Get Recommended Music List</div>
+      <table>
+        <thead>
+          <tr>
+            <th>제목</th>
+            <th>아티스트</th>
+            <th>링크</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tracks.map((track) => (
+            <tr key={track.id}>
+              <td>{track.name}</td>
+              <td>{track.artists[0].name}</td>
+              <td>
+                <a href={track.external_urls.spotify} target="_blank" rel="noreferrer">
+                  링크
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 Spotify.propTypes = {
